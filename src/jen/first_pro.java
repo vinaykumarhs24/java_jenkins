6a2ae7c919143105960f25affe1e7054da8fd641
AUTBLT410 <AUTBLT410@LT410-56.allianceblr.com>
2024-10-30 15:44:03 +0530

    This is the java project for jenkins

---
+++ src/jen/first_pro.java
@@ -0,0 +1,14 @@
+package jen;
+import java.util.Scanner;
+public class first_pro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Jenkin project + eclipse");
+		System.out.println("by VINAY KUMAR H S, Alliance University");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt(),b=sc.nextInt();
+		System.out.println("The sum and product of enter two numbers = " + (a+b) + " and " + (a*b));
+	}
+
+}
